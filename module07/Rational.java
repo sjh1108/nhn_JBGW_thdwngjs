2f43b1198808d8322b94d894dfa61b1702a33734
sjh1108 <ch6854@naver.com>
2024-08-26 16:00:09 +0900

    Ratioinal ing

--- module07/Rational.java
+++ module07/Rational.java
@@ -13,6 +13,75 @@ public class Rational{
         this.denominator = denominator / gCD;
     }
 
+    public Rational addRational(Rational left, Rational right){
+        int leftN = left.getNumerator();
+        int leftD = left.getDenominator();
+
+        int rightN = right.getNumerator();
+        int rightD = right.getDenominator();
+
+        int numerator = (leftN * rightD) + (leftD * rightN);
+        int denominator = leftD * rightD;
+
+        int gCD = gCD(numerator, denominator);
+        Rational tmp = new Rational(numerator/gCD, denominator/gCD);
+
+        return tmp;
+    }
+
+    public Rational subRational(Rational left, Rational right){
+        int leftN = left.getNumerator();
+        int leftD = left.getDenominator();
+
+        int rightN = right.getNumerator();
+        int rightD = right.getDenominator();
+
+        int numerator = (leftN * rightD) - (leftD * rightN);
+        int denominator = leftD * rightD;
+
+        int gCD = gCD(numerator, denominator);
+        Rational tmp = new Rational(numerator/gCD, denominator/gCD);
+
+        return tmp;
+    }
+
+    public Rational mulRational(Rational left, Rational right){
+        int leftN = left.getNumerator();
+        int leftD = left.getDenominator();
+
+        int rightN = right.getNumerator();
+        int rightD = right.getDenominator();
+
+        int numerator = leftN * rightN;
+        int denominator = leftD * rightD;
+
+        int gCD = gCD(numerator, denominator);
+        Rational tmp = new Rational(numerator/gCD, denominator/gCD);
+
+        return tmp;
+    }
+
+    public Rational divRational(Rational left, Rational right){
+        if(right.getNumerator() == 0){
+            throw new ArithmeticException("You can't divide by 0");
+        }
+        return mulRational(left, new Rational(right.getDenominator(), right.getNumerator()));
+    }
+
+    public int getNumerator(){
+        return this.numerator;
+    }
+    public int getDenominator(){
+        return this.denominator;
+    }
+
+    public void setNumerator(int numerator){
+        this.numerator = numerator;
+    }
+    public void setDenominator(int denominator){
+        this.denominator = denominator;
+    }
+
     private int gCD(int m, int n){
         return (n == 0) ? m : gCD(n, m % n);
     }
